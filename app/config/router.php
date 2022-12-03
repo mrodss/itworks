9f68bb7ded8756f2fd40a517b19c61ddb8f9869b
Matheus MR <mmussattorodrigues@gmail.com>
2022-12-03 17:42:31 -0300

    CHECAGEM CORRETA

--- app/config/router.php
+++ app/config/router.php
@@ -2,8 +2,6 @@
 
 #http://localhost:8081/
 #http://localhost:8081/home
-
-//EXEMPLO:
 $this->get('/', 'HomeController@home');
 $this->get('/home', 'HomeController@home');
-$this->get('/extrato', 'ContaController@extrato');
+// $this->get('/', 'ContaController@extrato');

e942bd731bd521c5074e63df5a705d0be2c3411d
Matheus MR <mmussattorodrigues@gmail.com>
2022-12-11 11:02:37 -0300

    EDITAR.TWIG.PHP

--- app/config/global.config.php
+++ app/config/global.config.php
@@ -6,5 +6,5 @@ define('DATABASE_HOST', 'localhost');
 define('DATABASE_USER', 'root');
 define('DATABASE_PASS', '123456');
 define('DATABASE_NAME', 'itworks');
-define('UNSET_URI_COUNT', 1);
+define('UNSET_URI_COUNT', 0);
 define('DEBUG_URI', true);
